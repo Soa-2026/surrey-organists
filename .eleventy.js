8fd6b9d699622d6c6b3c0863b339599d3499a064
Jonathan Church <jonathan@hatchd.co.uk>
2026-05-21 12:05:17 +0100

    Switch body fields to rich-text WYSIWYG, convert markdown content to HTML

--- .eleventy.js
+++ .eleventy.js
@@ -1,12 +1,6 @@
-const markdownIt = require("markdown-it");
-const md = markdownIt({ html: true, linkify: true, typographer: true });
-
 module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy({ "src/styles.css": "styles.css" });
   eleventyConfig.addPassthroughCopy({ "src/images": "images" });
-
-  eleventyConfig.addFilter("md", (content) => md.render(content || ""));
-  eleventyConfig.addFilter("mdInline", (content) => md.renderInline(content || ""));
 
   return {
     dir: {
